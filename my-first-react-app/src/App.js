035f59f47067f130adb5899f3397d22156e3bb02
786-JIYA <jiyas9610@gmail.com>
2025-10-26 16:31:55 +0530

    26-end

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -2,6 +2,11 @@ import './App.css';
 import {useState} from 'react';
 function App() {
   const [name,setName] = useState("Jiya");
+  const [events,setEvents] = useState([
+    {title:"Birthday Party",id:1},
+    {title:"Conference",id:2},
+    {title:"Wedding",id:3}
+  ]);
   const handleClick=()=>{
     setName("Arshiya");
     console.log(name);
@@ -10,6 +15,13 @@ function App() {
     <div className="App">
     <h1>My name is {name}</h1>
     <button onClick={handleClick}>Change name</button>
+    {
+      events.map((event,idx)=>(
+        <div key={event.id}>
+          <h2>{idx}-{event.title}</h2>
+        </div>
+      ))
+    }
     </div>
   );
 }
