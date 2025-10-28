56298d02499ff7aadb77bebadc7f8e75437e173f
786-JIYA <jiyas9610@gmail.com>
2025-10-28 22:04:59 +0530

    end

---
+++ my-first-react-app/src/components/Title.js
@@ -0,0 +1,10 @@
+export default function Title() {
+  return
+  <div>
+    <h1 className="title">Hello World from Title Component
+  </h1>
+  <br />
+  <h2 className="subTitle">Hello</h2>
+  </div>
+
+}
